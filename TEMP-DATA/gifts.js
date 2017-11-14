d64c08b0e7466097e322063a55a11109c07ee2d3
aakagi <alexanderakagi@gmail.com>
2017-11-13 17:04:02 -0800

    add temp data temporarily and will then gitignore again

---
+++ TEMP-DATA/gifts.js
@@ -0,0 +1,50 @@
+export default [{
+  datePublished: 'November 13, 2017',
+  version: 0.1,
+  categories: [],
+  text: 
+`Alex Akagi's Gift List, in order from easy to hard
+- Apples (just like, randomly in passing. also peaches)
+- Sushi (really more like sashimi, and also add una-don)
+- Teach me something
+- Example Change
+- Art that I like
+- Whiskey (really all alcohol, I generally prefer stronger stuff)
+- Substances.
+- Spending time with pretty girls (uhhh I really like pretty girls...)
+- Introductions to incredible people who you think I have to meet
+- Labor (depends on what I'm up to in life, but always looking to hire my talented friends)
+- Politicians (gotta get shit moving on a gvt level)
+- Professional Orchestra (also, I want to conduct dvorak 9 someday)
+- Maintain public infrastructure (shit's starting to fall apart and it would be great if someone just patched it up)
+- Automated commutes (automate all driving, humans driving is dangerous!)
+- Solutions for living forever (of course this includes staying youthful and also quick death-recovery would be nice)
+- Safe & fast interstellar travel (this includes multi-planetary, but Elon is already on it)
+- Find a way to get our brains to directly interface with computers (what Elon calls "neural lace")
+- Make breakthroughs in human understanding of physics
+- Become homies with interstellar beings`,
+}, {
+  datePublished: 'October 23, 2017',
+  version: 0.0,
+  categories: [],
+  text: 
+`Alex Akagi's Gift List, in order from easy to hard
+- Apples (just like, randomly in passing. also peaches)
+- Sushi (really more like sashimi, and also add una-don)
+- Teach me something
+- Art that I like
+- Whiskey (really all alcohol, I generally prefer stronger stuff)
+- Substances.
+- Spending time with pretty girls (uhhh I really like pretty girls...)
+- Introductions to incredible people who you think I have to meet
+- Labor (depends on what I'm up to in life, but always looking to hire my talented friends)
+- Politicians (gotta get shit moving on a gvt level)
+- Professional Orchestra (also, I want to conduct dvorak 9 someday)
+- Maintain public infrastructure (shit's starting to fall apart and it would be great if someone just patched it up)
+- Automated commutes (automate all driving, humans driving is dangerous!)
+- Solutions for living forever (of course this includes staying youthful and also quick death-recovery would be nice)
+- Safe & fast interstellar travel (this includes multi-planetary, but Elon is already on it)
+- Find a way to get our brains to directly interface with computers (what Elon calls "neural lace")
+- Make breakthroughs in human understanding of physics
+- Become homies with interstellar beings`,
+}]
