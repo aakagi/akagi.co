faeee6fc7912b91f2238a96c622acbfe42cb94ab
aakagi <alexanderakagi@gmail.com>
2017-12-24 14:58:16 -0500

    ga missing quote

--- components/GoogleAnalyticsTag/index.js
+++ components/GoogleAnalyticsTag/index.js
@@ -8,7 +8,7 @@ const GoogleAnalyticsTag = ({ trackingId }) => (
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer',${trackingId});`}} />
+      })(window,document,'script','dataLayer','${trackingId}');`}} />
     </NextHead>
     <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId}" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
   </div>
