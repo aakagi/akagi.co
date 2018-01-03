35094e2f9005201b416f23f08bdd65cad35d3d71
aakagi <alexanderakagi@gmail.com>
2018-01-03 17:16:26 -0500

    added temp prod line for contact form

--- containers/AkagiSection/index.js
+++ containers/AkagiSection/index.js
@@ -2,6 +2,40 @@ import styled from 'styled-components'
 import ContactMe from './ContactMe'
 import { red } from 'utils/colors'
 
+// TODO: Move out of this component
+import jsonFetch from 'lib/jsonFetch'
+import formToJson from 'lib/formToJson'
+
+const API_BASE = process.env.NODE_ENV === 'production'
+  ? 'https://5ltiyzowya.execute-api.us-east-1.amazonaws.com/production'
+  : 'localhost:4000'
+
+// TODO: Move out of this component
+function onSubmit(e) {
+  e.preventDefault()
+  const { elements, id: formId } = e.target
+
+  // Generate the request body
+  const body = formToJson(elements)
+  jsonFetch('POST', `${API_BASE}/akagi-contact`, {
+    body: body,
+  })
+  .then(({ message, error }) => {
+    if (error) {
+      return alert(error)
+    } else {
+      alert(message)
+    }
+
+    // Clear form
+    document.getElementById(formId).reset()
+  })
+  .catch(err => {
+    console.error('err', err)
+    alert(err)
+  })
+}
+
 const SectionWrapper = styled.div`
   margin: 36px 0;
   text-align: center;
@@ -21,7 +55,7 @@ const ContactSubtext = styled.h3`
   padding: 24px 0;
 `
 
-const AkagiSection = ({ onSubmit }) => (
+const AkagiSection = () => (
   <SectionWrapper>
     <AkagiHeader>
       AKAGI<Red>.</Red>CO
