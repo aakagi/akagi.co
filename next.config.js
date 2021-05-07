a883ce688334ab2060f0dac7a573b7c5ec3c1774
Alex Akagi <alex@akagi.co>
2021-05-06 21:54:14 -0400

    add nextjs image defaults to config -- i don't actually know what they mean so i decided not to tweak them

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // defaults
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // defaults, idk what these sizes mean
   },
 }
