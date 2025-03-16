aabeb1ea4b6687c01fb759acc2748551b71c5b3b
Alex Akagi <alex@akagi.co>
2025-03-16 13:17:37 -0400

    update everything to deploy to cloudflare

---
+++ src/modules/home/akagi-kanji.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Kanji = () => {
+  return (
+    <Image width="96" height="96" src="/akagi-kanji.png" alt="赤木" priority />
+  );
+};
+
+export { Kanji };
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["gotham", "sans-serif"],
+        body: ["gotham", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
