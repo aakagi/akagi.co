6848791dc6ef24574019cc8b3c3e87f41f9a7fc1
Alex Akagi <alex@akagi.co>
2025-03-28 13:43:58 -0400

    Add aria label

--- src/modules/home/footer.tsx
+++ src/modules/home/footer.tsx
@@ -4,34 +4,40 @@ const footerLinks = [
   {
     href: "https://facebook.com/alexakagi",
     Icon: FaFacebookSquare,
+    ariaLabel: "Facebook",
   },
   {
     href: "https://twitter.com/akagi____",
     Icon: FaTwitter,
+    ariaLabel: "Twitter",
   },
   {
     href: "https://instagram.com/alexakagi",
     Icon: FaInstagram,
+    ariaLabel: "Instagram",
   },
   {
     href: "https://github.com/aakagi",
     Icon: FaGithub,
+    ariaLabel: "GitHub",
   },
   {
     href: "https://www.linkedin.com/in/aakagi/",
     Icon: FaLinkedin,
+    ariaLabel: "LinkedIn",
   },
 ];
 
 const Footer = () => {
   return (
     <footer className="flex">
-      {footerLinks.map(({ href, Icon }, index) => (
+      {footerLinks.map(({ href, Icon, ariaLabel }, index) => (
         <a
           key={"social-" + index}
           target="_blank"
           rel="noreferrer"
           href={href}
+          aria-label={ariaLabel}
           className="mx-3 text-2xl opacity-40 transition-opacity duration-100 hover:opacity-80 sm:mx-4"
         >
           <Icon />
